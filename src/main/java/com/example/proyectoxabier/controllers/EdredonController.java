6c1f932323c64a9b42e6eede4bfb07e3b87bd84d
XBC04 <xabilugris@gmail.com>
2024-12-10 19:30:01 +0100

    add articles

--- src/main/java/com/example/proyectoxabier/controllers/EdredonController.java
+++ src/main/java/com/example/proyectoxabier/controllers/EdredonController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.proyectoxabier.models.Edredon;
 import com.example.proyectoxabier.services.EdredonService;
@@ -20,5 +22,19 @@ public class EdredonController {
             List<Edredon> edredones = edredonService.obtenerTodos();
             model.addAttribute("edredones", edredones);
             return "edredones";
+    }
+
+    // Mostrar formulario para agregar un nuevo edredón
+    @GetMapping("/edredones/crear")
+    public String mostrarFormularioCrear(Model model) {
+        model.addAttribute("edredon", new Edredon());
+        return "crearEdredon";
+    }
+
+    // Manejar la creación de un nuevo edredón
+    @PostMapping("/edredones/crear")
+    public String crearEdredon(@ModelAttribute Edredon edredon) {
+        edredonService.guardarEdredon(edredon);
+        return "redirect:/edredones";
     }
 }
