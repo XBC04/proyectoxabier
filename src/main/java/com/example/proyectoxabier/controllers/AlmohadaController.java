65b1b99d91f0c5f5abb2df7f866e589c06cea6c6
XBC04 <xabilugris@gmail.com>
2024-12-10 20:18:58 +0100

    button add almohadas

--- src/main/java/com/example/proyectoxabier/controllers/AlmohadaController.java
+++ src/main/java/com/example/proyectoxabier/controllers/AlmohadaController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.proyectoxabier.models.Almohada;
 import com.example.proyectoxabier.services.AlmohadaService;
@@ -21,5 +23,17 @@ public class AlmohadaController {
         List<Almohada> almohadas = almohadaService.obtenerTodas();
         model.addAttribute("almohadas", almohadas);
         return "almohadas";
+    }
+
+    @GetMapping("/almohadas/crear")
+    public String mostrarFormularioCrear(Model model) {
+        model.addAttribute("almohada", new Almohada());
+        return "crearAlmohadas";
+    }
+
+    @PostMapping("/almohadas/crear")
+    public String crearAlmohada(@ModelAttribute Almohada almohada) {
+        almohadaService.guardarAlmohada(almohada);
+        return "redirect:/almohadas";
     }
 }
