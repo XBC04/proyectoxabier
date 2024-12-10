b54ffd0a7c8b14f328e1cd648bea0aaaa0c13dd0
XBC04 <xabilugris@gmail.com>
2024-12-10 20:02:19 +0100

    button add alfombra

--- src/main/java/com/example/proyectoxabier/controllers/AlfombraController.java
+++ src/main/java/com/example/proyectoxabier/controllers/AlfombraController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.proyectoxabier.models.Alfombra;
 import com.example.proyectoxabier.services.AlfombraService;
@@ -21,5 +23,17 @@ public class AlfombraController {
         List<Alfombra> alfombras = alfombraService.obtenerTodas();
         model.addAttribute("alfombras", alfombras);
         return "alfombras";
+    }
+
+    @GetMapping("/alfombras/crear")
+    public String mostrarFormularioCrear(Model model) {
+        model.addAttribute("alfombra", new Alfombra());
+        return "crearAlfombra";
+    }
+
+    @PostMapping("/alfombras/crear")
+    public String crearAlfombra(@ModelAttribute Alfombra alfombra) {
+        alfombraService.guardarAlfombra(alfombra);
+        return "redirect:/alfombras";
     }
 }
--- src/main/java/com/example/proyectoxabier/controllers/EdredonController.java
+++ src/main/java/com/example/proyectoxabier/controllers/EdredonController.java
@@ -24,14 +24,12 @@ public class EdredonController {
             return "edredones";
     }
 
-    // Mostrar formulario para agregar un nuevo edredón
     @GetMapping("/edredones/crear")
     public String mostrarFormularioCrear(Model model) {
         model.addAttribute("edredon", new Edredon());
         return "crearEdredon";
     }
 
-    // Manejar la creación de un nuevo edredón
     @PostMapping("/edredones/crear")
     public String crearEdredon(@ModelAttribute Edredon edredon) {
         edredonService.guardarEdredon(edredon);
